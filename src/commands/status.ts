5af831d63f2f59478817174796d92a214b993c3c
argonmining <woodashton@outlook.com>
2024-08-23 18:39:44 -0700

    restart

--- src/commands/status.ts
+++ src/commands/status.ts
@@ -1,7 +1,5 @@
 import { Message } from 'discord.js';
 import axios from 'axios';
-import fs from 'fs';
-import { EmbedBuilder } from '@discordjs/builders';
 
 export const handleStatusCommand = async (message: Message, args: string[]) => {
   if (args.length !== 1) {
@@ -10,24 +8,28 @@ export const handleStatusCommand = async (message: Message, args: string[]) => {
   }
 
   const token = args[0].toUpperCase();
-  const templateContent = fs.readFileSync('message_template.json', 'utf8');
-  const template = JSON.parse(templateContent);
 
   try {
     const response = await axios.get(`${process.env.API_BASE_URL}/token/${token}?stat=true&holder=true`);
     const tokenData = response.data.result[0];
 
-    const embed = new EmbedBuilder()
-      .setColor(template.color)
-      .setImage(template.background_images[0].url)
-      .setAuthor({ name: template.author.name, iconURL: template.author.icon_url })
-      .setFooter({ text: 'x.com/NachoWyborski' })
-      .setTitle(`Mint Status for ${token}`)
-      .addFields(
+    const embed = {
+      color: 7391162,
+      title: `Mint Status for ${token}`,
+      fields: [
         { name: 'Max Supply', value: tokenData.max, inline: true },
         { name: 'Minted', value: tokenData.minted, inline: true },
         { name: 'Holders', value: tokenData.holderTotal || 'N/A', inline: true }
-      );
+      ],
+      footer: { text: 'x.com/NachoWyborski' },
+      author: {
+        name: 'Nacho the 𐤊at',
+        icon_url: 'https://coinchimp-240602.nacho-react.pages.dev/nacho_bot_logo.png'
+      },
+      image: {
+        url: 'https://i.ibb.co/G0BvQCs/nachobot-banner1small.png'
+      }
+    };
 
     message.channel.send({ embeds: [embed] });
   } catch (error) {
